e2da849e836056a20e5897278ed617d7fc7c5440
Loi Pham <loitpham@gmail.com>
2021-04-24 00:00:02 -0700

    Added get conditions API call.

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
 const apiKey = 'IGkZ4yh6vkRRoyMFXz2DsvX3CZ70NRXM';
 
+// get city information
 const getCity = async (city) => {
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${apiKey}&q=${city}`
     const response = await fetch(base + query);
-    return await response.json();
+    const data = await response.json();
+    return data[0];
 };
 
+// get weather information
+const getCurrentConditions = async (location) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${location}?apikey=${apiKey}`;
+    const response = await fetch(base + query);
+    return await response.json();
+}
+
 getCity('seattle')
-    .then(data => console.log(data[0].Key))
+    .then(cityData => getCurrentConditions(cityData.Key))
+    .then(conditionsData => console.log(conditionsData))
     .catch(err => console.log(err));
